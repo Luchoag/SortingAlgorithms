98c65c3333753079300b31b39fb8574b6ddefccd
Luciano Greppi <luchoag@gmail.com>
2019-09-28 13:07:04 -0300

    Add files via upload

---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortingAlgorithms;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 20, 58, 16, 18, 33 };
+            Console.WriteLine("--------Original Array------");
+            printArray(arr);
+            Console.WriteLine("--------Bubble Sort------");
+            SortingAlgorithms.BubbleSort(arr);
+            printArray(arr);
+            Console.WriteLine("------Insertion Sort--------");
+            assignRandomValuesToArray(arr);
+            SortingAlgorithms.InsertionSort(arr);
+            printArray(arr);
+            Console.WriteLine("------Selection Sort--------");
+            assignRandomValuesToArray(arr);
+            printArray(arr);
+            Console.WriteLine("-------");
+            SortingAlgorithms.SelectionSort(arr);
+            printArray(arr);
+            Console.ReadKey();
+
+        }
+
+        public static void printArray (int[] arr) {
+            for (int i=0; i<arr.Length ; i++) {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+
+        /*assignRandomValuesToArray(int [] arr):
+         * Randomly assigns int values from 0 to 99 to the array so we can properyl check the
+         * differents sorts are working as expected.
+         * */
+        public static void assignRandomValuesToArray(int [] arr) {
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(0, 100);
+            }
+        }        
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithms.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public static class SortingAlgorithms
+    {
+        /*
+         * BubbleSort Algorithm: it's the simplest sorting algorithm.
+         * Repeatedly steps through the list, compares two adjancets
+         * elements and swaps them if they are in the wrong order.
+         * */
+        public static void BubbleSort(int [] arr) {
+
+            bool swapped;
+            for (int i=0; i<arr.Length; i++) {
+                swapped = false;
+                for (int j=0; j<arr.Length-i-1; j++) {
+                    if (arr[j]>arr[j+1]) {
+                        int temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped) break;
+            }
+        }
+        /*
+         * Insertion Sort Algorithm: sorts a list of elements by 
+         * taking each element and inserting in the corresponding
+         * position to the left.
+         * */
+        public static void InsertionSort(int [] arr) {
+            int j;
+            for (int i=1; i<arr.Length; i++) {
+                j = i;
+                while (j>0 && arr[j-1] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    j = j - 1;
+                }
+            }
+        }
+
+        /*
+         *Selection Sort: sorts a list of elements by taking the smallest element and
+         *swapping it with the element to the left.
+         * */
+        public static void SelectionSort(int [] arr) {
+            for (int k=0; k<arr.Length-1; k++) {
+                int minimumIndex = k;
+                for (int j = k+1; j<arr.Length; j++) {
+                    if (arr[j] < arr[minimumIndex])
+                        minimumIndex = j;
+                }
+                    int temp = arr[minimumIndex];
+                    arr[minimumIndex] = arr[k];
+                    arr[k] = temp;
+            }
+        }
+    } //end SortingAlgorithms
+} //end namespace
